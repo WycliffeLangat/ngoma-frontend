e1638c203269148f82209ea8011d34b4ccaeee7f
Wycliffe Langat <wickielangat99@gmail.com>
2026-06-08 17:05:01 +0300

    Improve mobile premium charts layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
 import "./index.css";
-import "./styles/ngomaTheme.css";
+import "./styles/mobilePremiumFixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
